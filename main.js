60b5fb8869c3d826dc64e08d7657f5eadf4975e3
Arkadii-M <70690857+Arkadii-M@users.noreply.github.com>
2021-12-21 20:19:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,176 @@
+host_ip = 'http://192.168.1.7/';
+
 function RenderContent()
 {
-    div_content = document.getElementById("content");
-    alert(123);
-    div_content.innerHTML =" <form  action=\"?time=time\" >Time in s:<br><input type=\"text\" name=\"time\" value=\"0\"><br><input type=\"submit\" value=\"Start\"><p>Countdown timer:<p id=\"cdown\">-</p></p></form> <form action=\"/StopTimer\">Off led:<input type=\"submit\" value=\"Stop\"></form>";
-    OnTimerStart();
+    //Add handlers
+    exp_btn = document.getElementById("StartTimer")
+    stop_btn = document.getElementById("StopTimer")
+    exp_btn.onclick = StartTimer;
+    stop_btn.onclick = StopTimer;
+    GetLedStatus();
+    GetTimerStatus();
+}
+
+function GetTimerStatus()
+{
+    fetch(host_ip + "api/GetTimerStatus")
+      .then((response) => {
+        return response.text();  })
+    .then((data) => {
+        console.log(data);
+        if(data == "RUNNING")
+        {
+            RefreshPageTimer();
+        }
+    });
+
+}
+function RefreshPageTimer()
+{
+    fetch(host_ip + "api/GetTimerTime")
+      .then((response) => {
+        return response.text();  })
+    .then((data) => {
+        console.log(data);
+        StartPageTimer(Math.round(Number(data)/1000));
+    });
+}
+function StartTimer()
+{
+    expose_time = document.getElementById("expose_time");
+    fetch(host_ip + "api/StartTimer",{
+        mode:'no-cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({time:expose_time.value})
+      })
+      .then((response) => {
+        return response.text();  })
+    .then((data) => {
+        console.log(data);
+    });
+    StartPageTimer(expose_time.value);
+    GetLedStatus();
+}
+
+function StopTimer()
+{
+    fetch(host_ip + "api/StopTimer",{
+        mode:'no-cors',
+        method: 'POST'}).then((response) => {
+        return response.text();  })
+    .then((data) => {
+        console.log(data);
+    });
+    GetLedStatus();
+    StopPageTimer();
+}
+
+function GetLedStatus()
+{
+    led_status = document.getElementById("led_status");
+    fetch(host_ip + "api/GetLedStatus").then((response) => {
+        return response.text();  })
+    .then((data) => {
+        console.log(data);
+        if(data == "ON")
+        {
+            led_status.innerHTML = "ON";
+            led_status.style.color = 'red';
+        }
+        else
+        {
+            led_status.innerHTML = "OFF";
+            led_status.style.color = 'green';
+        }
+    }).catch((e) => {
+        console.log('Error: ' + e.message);
+        console.log(e.response);
+    });
 }
 
 
-function OnTimerStart()
+// Page timer
+
+var timer = 0;
+var timer_interval;
+function StartPageTimer(seconds)
 {
-    const queryString = window.location.search;
-    var searchParams = new URLSearchParams(queryString);
-    let time_s = Number(searchParams.get('time'))
-    if(!isNaN(time_s))
-    {
-        RenderTimer(Number(searchParams.get('time')))
-    }
+    timer = seconds;
+    timer_interval =setInterval(RenderPageTimer,1000);
 }
-function RenderTimer(seconds)
+
+function StopPageTimer()
 {
-    if(seconds == 0)
+    clearInterval(timer_interval);
+    el = document.getElementById("cdown");
+    el.innerHTML = "off";
+}
+function RenderPageTimer()
+{
+    if(timer == 0)
     {
-        OnTimerEnd();
+        OnPageTimerEnd();
+        StopPageTimer();
         return;
     }
     else
     {
         el = document.getElementById("cdown");
-        el.innerHTML = seconds;
-        setTimeout(RenderTimer, 1000, (seconds-1));
-
+        el.innerHTML = timer;
+        timer-=1;
     }
 }
 
-function OnTimerEnd()
+function OnPageTimerEnd()
 {
     el = document.getElementById("cdown");
-    el.innerHTML = "-";
-
+    el.innerHTML = "off";
+    window.navigator.vibrate(200);
     console.log("Timer up!")
 }
+// function OnTimerStart()
+// {
+//     const queryString = window.location.search;
+//     var searchParams = new URLSearchParams(queryString);
+//     let time_s = Number(searchParams.get('time'))
+//     if(!isNaN(time_s))
+//     {
+//         RenderTimer(Number(searchParams.get('time')))
+//     }
+// }
 
-function OnTimerCanceled()
-{
-    el = document.getElementById("cdown");
-    el.innerHTML = "-";
-    console.log("Timer canceled!")
-}
+// function RenderTimer(seconds)
+// {
+//     if(seconds == 0)
+//     {
+//         OnTimerEnd();
+//         return;
+//     }
+//     else
+//     {
+//         el = document.getElementById("cdown");
+//         el.innerHTML = seconds;
+//         setTimeout(RenderTimer, 1000, (seconds-1));
+//     }
+// }
+// function CancelTimer()
+// {
+
+// }
+
+// function OnTimerEnd()
+// {
+//     el = document.getElementById("cdown");
+//     el.innerHTML = "off";
+//     console.log("Timer up!")
+// }
+
+// function OnTimerCanceled()
+// {
+//     el = document.getElementById("cdown");
+//     el.innerHTML = "off";
+//     console.log("Timer canceled!")
+// }
